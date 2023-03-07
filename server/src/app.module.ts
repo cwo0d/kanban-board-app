e7f012e44d27e43d2e510fdfa22d6679db630a61
Ilya Ovchinnikov <christopher.wo0d@icloud.com>
2023-03-07 16:55:23 +0500

    12345

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+import { User } from './users/users.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	controllers: [],
+	providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+		}),
+		SequelizeModule.forRoot({
+			dialect: 'postgres',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			models: [User],
+			autoLoadModels: true,
+		}),
+		UserModule,
+	],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const PORT = process.env.APP_PORT || 9000;
+
+	const app = await NestFactory.create(AppModule);
+	await app.listen(PORT, () => {
+		console.log(`Server started on port ${PORT}`);
+	});
 }
 bootstrap();
